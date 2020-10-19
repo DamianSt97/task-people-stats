c4ca40548af9f37af8c97279dbe1f757526b5962
stawiarski_01 <stawiarski1504@gmail.com>
2020-10-19 17:10:18 +0200

    Trzeci Commit

--- Main.java
+++ Main.java
@@ -7,8 +7,20 @@ class Main {
 }
 
 class PeopleStats {
+    private final List<Person> people;
 
+    public PeopleStats(Path inputFilePath) {
+        try {
+            people = Files.lines(inputFilePath)
+                    .map(line -> line.split("\t"))
+                    .map(chunks -> new Person(chunks[0], chunks[1]))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 }
+
 
 class Person {
     private final String firstName;
