e099b203487e8a51003bf745c095b9d933bda274
stawiarski_01 <stawiarski1504@gmail.com>
2020-10-19 17:09:15 +0200

    Drugi Commit

--- Main.java
+++ Main.java
@@ -12,9 +12,20 @@ class PeopleStats {
 
 class Person {
     private final String firstName;
+    private final String LastName;
+
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        LastName = lastName;
+    }
 
     public Person(String firstName) {
         this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
     }
 
     public String getFirstName() {
