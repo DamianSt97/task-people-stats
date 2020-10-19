ecca476ec010116e903091c34bcb7788e21c1b2f
stawiarski_01 <stawiarski1504@gmail.com>
2020-10-19 17:04:34 +0200

    Pierwszy commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
+
+class PeopleStats {
+
+}
+
+class Person {
+    private final String firstName;
+
+    public Person(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
